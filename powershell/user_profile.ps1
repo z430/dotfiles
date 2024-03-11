43367bbe6e1ed19796f33df0f2dff30a6c771617
z430 <maystya@gmail.com>
2024-03-11 21:20:08 +0700

    feat: add autopaired

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -101,3 +101,25 @@ Register-ArgumentCompleter -CommandName ssh, scp, sftp -Native -ScriptBlock {
 	| Where-Object { $_ -like "${textToComplete}*" } `
 	| ForEach-Object { [CompletionResult]::new((&$generateCompletionText($_)), $_, [CompletionResultType]::ParameterValue, $_) }
 }
+
+Set-PSReadLineKeyHandler -Chord '"', "'" `
+	-BriefDescription SmartInsertQuote `
+	-LongDescription "Insert paired quotes if not already on a quote" `
+	-ScriptBlock {
+	param($key, $arg)
+
+	$line = $null
+	$cursor = $null
+	[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+	if ($line.Length -gt $cursor -and $line[$cursor] -eq $key.KeyChar) {
+		# Just move the cursor
+		[Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+	}
+	else {
+		# Insert matching quotes, move cursor to be in between the quotes
+		[Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)" * 2)
+		[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+		[Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor - 1)
+	}
+}
